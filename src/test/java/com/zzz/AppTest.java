066708c9850850775b6d6aa5305edd93e3a75936
bj <bj@126.com>
2021-12-21 22:49:03 +0800

    初始化

---
+++ src/test/java/com/zzz/AppTest.java
@@ -0,0 +1,18 @@
+package com.zzz;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void test01(){
+        System.out.println("1.........");
+    }
+}
+
